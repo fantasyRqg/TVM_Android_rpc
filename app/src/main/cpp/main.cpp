06dccec2ed5d579115dc9ba9ec27f927f6d8a656
rqg <ranqingguo318@gmail.com>
2021-01-07 17:52:47 +0800

    compile tvm runtime from source

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -4,7 +4,6 @@
 
 
 #include <jni.h>
-#include <3rdparty/dmlc-core/include/dmlc/logging.h>
 #include <dmlc/logging.h>
 #include "rpc_server.h"
 
@@ -20,7 +19,7 @@ void start_rpc(JNIEnv *env, jclass, jint port, jstring _tracker_addr, jstring _c
 
     auto tracker_addr = env->GetStringUTFChars(_tracker_addr, nullptr);
     auto custom_addr = env->GetStringUTFChars(_custom_addr, nullptr);
-    tvm::runtime::RPCServerCreate("0.0.0.0", port, port + 100, tracker_addr, "android", custom_addr, false);
+    tvm::runtime::RPCServerCreate("0.0.0.0", port, port + 100, tracker_addr, "android64", custom_addr, false);
 
     env->ReleaseStringUTFChars(_tracker_addr, tracker_addr);
     env->ReleaseStringUTFChars(_custom_addr, custom_addr);
